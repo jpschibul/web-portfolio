c738a6503314f6bfbc92ef074f559f1ec214c1aa
Judith Pschibul <judithpschibul@judiths-air.home>
2025-03-15 14:19:29 +0000

    added hero section

--- src/components/SplineTest.tsx
+++
@@ -1,9 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function SplineTest() {
-  return (
-    <div>
-      <Spline scene="https://prod.spline.design/Fg7EGE2EOh5wXqr1/scene.splinecode" />
-    </div>
-  );
-}
